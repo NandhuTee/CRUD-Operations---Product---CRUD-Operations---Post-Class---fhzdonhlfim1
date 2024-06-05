341270b1421634e795fd9d8cc15dbe93aafe14da
Nandhini_T <129613480+NandhuTee@users.noreply.github.com>
2024-06-05 22:54:42 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const productRouter = require('./controllers/productControllers');
+const productRouter = require('../controllers/productControllers');
 //const router = require('../controllers/productControllers');
 
 //Router Middlewares
