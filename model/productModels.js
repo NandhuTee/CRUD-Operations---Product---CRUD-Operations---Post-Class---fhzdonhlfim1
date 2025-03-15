3ec73f43d0782a29ef5a19f5cd8cb4f70353950a
Nandhini_T <129613480+NandhuTee@users.noreply.github.com>
2025-03-15 21:34:57 +0530

    Update productModels.js

--- model/productModels.js
+++ model/productModels.js
@@ -1,16 +1,9 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    productName: {
-        type: String,
-        required: true
-    },
-    price: {
-        type: Number,
-        required: true
-    }
-}, { timestamps: true });
+    productName: { type: String, required: true },
+    price: { type: Number, required: true }
+});
 
 const Product = mongoose.model('Product', productSchema);
-
 module.exports = Product;
